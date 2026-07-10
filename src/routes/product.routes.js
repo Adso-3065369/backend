b61a4f8410ed1cbe6f5667f450f9747cde31c436
yimmersamith <yimmersamithduarteplata624@gmail.com>
2026-07-10 17:47:18 -0500

    feat: seccion de recupercion de contraseña casi terminado

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -26,59 +26,27 @@ export const productRouter = Router();
  */
 productRouter.get(
   "/", 
-  verifyToken, 
-  checkPermission("products.index"), 
   ProductController.getAll
 );
 
-/**
- * @route GET /:id
- * @description Consulta detallada de un producto específico.
- * @access Privado (Requiere permiso 'products.view')
- */
 productRouter.get(
   "/:id", 
-  verifyToken, 
-  checkPermission("products.view"), 
   ProductController.getById
 );
 
-/**
- * @route POST /
- * @description Registra un nuevo producto en el inventario.
- * @access Privado (Requiere permiso 'products.create')
- * @middleware Valida el cuerpo de la petición contra 'productSchema'.
- */
 productRouter.post(
   "/", 
-  verifyToken, 
-  checkPermission("products.create"), 
   validateSchema(productSchema), 
   ProductController.create
 );
 
-/**
- * @route PUT /:id
- * @description Actualiza la información de un producto existente.
- * @access Privado (Requiere permiso 'products.update')
- * @middleware Valida el cuerpo de la petición contra 'productSchema'.
- */
 productRouter.put(
   "/:id", 
-  verifyToken, 
-  checkPermission("products.update"), 
   validateSchema(productSchema), 
   ProductController.update
 );
 
-/**
- * @route DELETE /:id
- * @description Elimina un registro de producto del sistema.
- * @access Privado (Requiere permiso 'products.delete')
- */
 productRouter.delete(
   "/:id", 
-  verifyToken, 
-  checkPermission("products.delete"), 
   ProductController.delete
 );
