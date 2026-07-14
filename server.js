1dc59e4779de3b3bdcc6333634346c678872288d
Javid495 <juandavidcacereshernandez@gmail.com>
2026-07-14 11:12:34 -0500

    fix: Se reestablecio el puerto por defecto como estaba

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from "./src/app.js";
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor encendido en el puerto ${PORT}`);
